a5faa91ddcc36cebcad8f6f2145dad3b553d03f5
Jayson Falkner <jfalkner@gmail.com>
2020-10-10 22:10:51 -0400

    CAS-based spinlock

--- src/spinlock.h
+++ src/spinlock.h
@@ -6,5 +6,12 @@
  *
  * Do not use this in lockfree algorithms. It is still effectively a user-space mutex.
  */
+#ifndef JFALKNER_SPINLOCK_H
+#define JFALKNER_SPINLOCK_H
+
+
 #define acquire_lock(lock) while (!__sync_bool_compare_and_swap(&(lock), 0, 1));
 #define release_lock(lock) __sync_bool_compare_and_swap(&(lock), 1, 0);
+
+
+#endif // JFALKNER_SPINLOCK_H
